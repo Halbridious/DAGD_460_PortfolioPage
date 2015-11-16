e6c7afd2afad9c6ad1c3fbd8feefa3d3eaa9ec56
Jacob Hamberg <halbridious@gmail.com>
2015-11-16 02:09:18 -0500

    Synced the right folder this time
    Push new pages, major update to home page content, wordpress  which I'm
    still not sure how i'm using.
    
    +CSS
    +Main landing pages for each menu area
    +folder structure
    +Wordpress content (not yet used for anything)

---
+++ contact.php
@@ -0,0 +1,37 @@
+<?php
+//Home page
+//Jacob Hamberg
+//Project: DAGD 460 Final (Professor Nick Pattison)
+
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Hamworks homepage</title>
+    <link href="style/grid.css" rel="stylesheet">
+</head>
+<body>
+    <div id="body">
+        <header id="head" class="col5">
+            <a href="home.php">Jacob Hamberg: The Website</a>
+        </header>
+        <nav id="mainNav" class="col5">
+            <ul id = "navList">
+                <li id="blog" class="col1"><a href="blog.php">BLOG</a></li>
+                <li class="col1"></li>
+                <li id="contact" class="col1 current"><a href="contact.php" class="current">CONTACT</a></li>
+                <li class="col1"></li>
+                <li id="portfolio" class="col1"><a href="portfolio.php">PORTFOLIO</a></li>
+            </ul>         
+        </nav>
+        <div id="backdrop" class="col5">
+            <div id="twitter">
+                <a class="twitter-timeline" data-dnt="true" href="https://twitter.com/Halbridious" data-widget-id="666081418607366144" width="200">Tweets by @Halbridious</a>
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,73 @@
+<?php
+//Home page
+//Jacob Hamberg
+//Project: DAGD 460 Final (Professor Nick Pattison)
+
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Hamworks homepage</title>
+    <link href="style/grid.css" rel="stylesheet">
+</head>
+<body>
+    <div id="body">
+        <header id="head" class="col5">
+            <a href="home.php" class="current">Jacob Hamberg: The Website</a>
+        </header>
+        <nav id="mainNav" class="col5">
+            <ul id = "navList">
+                <li id="blog" class="col1"><a href="blog.php">BLOG</a></li>
+                <li class="col1"></li>
+                <li id="contact" class="col1"><a href="contact.php">CONTACT</a></li>
+                <li class="col1"></li>
+                <li id="portfolio" class="col1"><a href="portfolio.php">PORTFOLIO</a></li>
+            </ul>         
+        </nav>
+        <div id="backdrop" class="col5">
+            <div class="col1 picture empty"></div>
+            <div class="col1 picture"><a href="imgs/Dapper-Finery.jpg" class="portLink" ><img src="imgs/Dapper-Finery.jpg"></a></div>
+            <div class="col1 picture"><a href="imgs/Dapper-Finery.jpg" class="portLink" ><img src="imgs/Dapper-Finery.jpg"></a></div>
+            <div class="col1 picture"><a href="imgs/Dapper-Finery.jpg" class="portLink" ><img src="imgs/Dapper-Finery.jpg"></a></div>
+            <div class="col1 picture empty"></div>
+            
+            <?
+            /*
+            *PHP LOOP is going to find total number of elements in the portfolio library
+            *starting with an empty column, it will make a box for the header image of each element.  
+            *every 3rd element will be followed by TWO "col1" elements for spacing
+            *
+            *the picture should be a random snapshot of the element
+            */
+            
+            /*psuedocode until i figure out how to integrate wordpress
+            *
+            *$portfolioArray-> get portfolio table length
+            *
+            *for ($i = 0; i < $portfolioArray.length(); i++){
+            *   insert <div class="col1 picture"><a href="$portfolioArray[i] page link" class="portLink" >
+            *   <img src="$portfolioArray[i].img"></a></div>
+            *
+            *   if (i/3 % 0){
+            *       insert <div class="col1 picture empty">
+            *       insert <div class="col1 picture empty">
+            *       
+            *   }
+            *
+            *
+            *}
+            *
+            *
+            *
+            *
+            *
+            *
+            */            
+            ?>
+            
+        </div>
+    </div>
+
+</body>
+</html>
